8b864eac9ed2dd57ad03fb332aa4b0745451de38
mrxart <60448395+mrxart@users.noreply.github.com>
2020-02-23 15:49:47 +0200

    25
    25

---
+++ Hometasks/lesson__25/src/js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+	 $('.toggles button').click(function() {
+	 	 var get_id = this.id;
+	 	 var get_current = $('.posts .' + get_id);
+	 	 $('.post').not(get_current).hide(500);
+	 	 get_current.show(500);
+	 });
+	 $('#all').click(function() {
+	 	 $('.post').show(500);
+	 });
+});
